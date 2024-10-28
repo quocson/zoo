4d618d589302db9861df8c004d1210a7a444879e
Steve (Son Nguyen) <darklord.nlqs@gmail.com>
2024-10-28 15:57:31 +0800

    update client rendering

--- client/zoo-client/src/app/layout.tsx
+++ client/zoo-client/src/app/layout.tsx
@@ -1,36 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Authenticator } from '@aws-amplify/ui-react';
-import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId: "ap-northeast-1_UIKUJ27Hp",
-      userPoolClientId: "66rkt2qrjlokagfl6ssc4of7p1",
-      identityPoolId: "ap-northeast-1:887f92f3-1b5b-441b-9f42-0c72eb4109e1",
-      loginWith: {
-        email: true,
-      },
-      signUpVerificationMethod: "code",
-      userAttributes: {
-        email: {
-          required: true,
-        },
-      },
-      allowGuestAccess: true,
-      passwordFormat: {
-        minLength: 8,
-        requireLowercase: true,
-        requireUppercase: true,
-        requireNumbers: true,
-        requireSpecialCharacters: true,
-      },
-    },
-  },
-})
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
--- client/zoo-client/src/app/login/page.tsx
+++ client/zoo-client/src/app/login/page.tsx
@@ -1,5 +1,34 @@
+'use client'
 import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "ap-northeast-1_UIKUJ27Hp",
+      userPoolClientId: "66rkt2qrjlokagfl6ssc4of7p1",
+      identityPoolId: "ap-northeast-1:887f92f3-1b5b-441b-9f42-0c72eb4109e1",
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+})
 
 export default function App() {
   return (
